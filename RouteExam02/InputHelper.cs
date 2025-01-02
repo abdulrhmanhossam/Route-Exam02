2969e7eb8fc44d5491b6eb1022f0c6a325cbd9d8
abdulrahman <abdulrahmn398@outlook.com>
2025-01-02 12:10:43 +0200

    add InputHelper class to validate inputs and implement subject class

---
+++ RouteExam02/InputHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RouteExam02
+{
+    public static class InputHelper
+    {
+        // this method will read an integer from the console
+        public static int ReadInt(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        // this method will read a string from the console
+        public static string ReadString(string prompt)
+        {
+            string result;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                result = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Input cannot be empty.");
+            }
+        }
+    }
+}
--- RouteExam02/Subject.cs
+++ RouteExam02/Subject.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RouteExam02
+﻿namespace RouteExam02
 {
     public class Subject
     {
+        public Subject(int subjectId, string subjectName)
+        {
+            SubjectId = subjectId;
+            SubjectName = subjectName;
+        }
+
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public Exam Exam { get; set; }
+
+        // take any thing type of exam
+        public void CreateExam(Exam exam)
+        {
+            Exam = exam;
+        }
+
+        public void ShowExam()
+        {
+            Exam.StartExam();
+        }
     }
 }

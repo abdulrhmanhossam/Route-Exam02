9f5f73b8e64959081d6c2c44ea824b5764815caa
abdulrahman <abdulrahmn398@outlook.com>
2025-01-02 11:01:35 +0200

    Implement Answer Class

--- RouteExam02/Answer.cs
+++ RouteExam02/Answer.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RouteExam02
+﻿namespace RouteExam02
 {
     public class Answer
     {
+        public Answer(int answerId, string answerText)
+        {
+            AnswerId = answerId;
+            AnswerText = answerText;
+        }
+
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+
+        public override string ToString()
+        {
+            return AnswerText;
+        }
     }
 }

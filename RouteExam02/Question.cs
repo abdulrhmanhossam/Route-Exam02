911fab8e032d57ec525bb2ffd414f435bdbe9f7f
abdulrahman <abdulrahmn398@outlook.com>
2025-01-02 11:14:01 +0200

    implement question class

--- RouteExam02/Question.cs
+++ RouteExam02/Question.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 namespace RouteExam02
 {
-    public class Question
+    public abstract class Question : ICloneable, IComparable
     {
-        
+        public Question(string header, string body, int mark, Answer[] answers, Answer correctAnswer)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+            Answers = answers;
+            CorrectAnswer = correctAnswer;
+        }
+
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public Answer[] Answers { get; set; }
+        public Answer CorrectAnswer { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone(); // create a shallow copy from the current object
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) 
+                return 1;
+
+            Question? otherQuestion = obj as Question;
+
+            return otherQuestion != null ? 
+                Mark.CompareTo(otherQuestion.Mark) : 0;
+        }
+
+        public abstract bool IsCorrect(string answer);
+
+
+        public override string ToString()
+        {
+            return $"{Header}\n{Body}\nMarks: {Mark}";
+        }
     }
 }

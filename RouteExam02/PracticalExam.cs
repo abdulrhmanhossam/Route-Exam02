3d895bbc99ea739b8714a37f5caf06d3f2187b17
abdulrahman <abdulrahmn398@outlook.com>
2025-01-02 11:56:32 +0200

    implement PracticalExam class

--- RouteExam02/PracticalExam.cs
+++ RouteExam02/PracticalExam.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RouteExam02
+﻿namespace RouteExam02
 {
-    public class PracticalExam
+    public class PracticalExam : Exam
     {
+        public PracticalExam(DateTime examTime, int numberOfQuestions, Question[] questions)
+            : base(examTime, numberOfQuestions, questions)
+        {
+
+        }
+
+        public override void StartExam()
+        {
+            foreach (var question in Questions)
+            {
+                Console.WriteLine(question);
+            }
+            
+            Console.WriteLine("\nThe right answers are shown after the exam.");
+        }
     }
 }

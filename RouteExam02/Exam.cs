23e2b2b12c21d687b59c322c86a3ac6f8cb9faa1
abdulrahman <abdulrahmn398@outlook.com>
2025-01-02 11:39:20 +0200

    implement Exam class

--- RouteExam02/Exam.cs
+++ RouteExam02/Exam.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RouteExam02
+﻿namespace RouteExam02
 {
-    public class Exam
+    public abstract class Exam
     {
+        protected Exam(DateTime examTime, int numberOfQuestions, Question[] questions)
+        {
+            ExamTime = examTime;
+            NumberOfQuestions = numberOfQuestions;
+            Questions = questions;
+        }
+
+        public DateTime ExamTime { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public Question[] Questions { get; set; }
+
+        public abstract void StartExam();
+
     }
 }
